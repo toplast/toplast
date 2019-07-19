f2439b02c75068a998f445340b96cc70f8813cfa
Henrique de Castilhos <henrique.castilhos@softexpert.com>
2019-07-19 15:44:00 -0300

    :sparkles: Last.fm services and config file with the env variables

---
+++ functions/services/lastfm.js
@@ -0,0 +1,31 @@
+const axios = require('axios')
+const config = require('./../config')
+
+const paramsToString = (params) => {
+  const entries = Object.entries(params)
+
+  let query = ''
+  for (const [param, value] of entries) {
+    query += `&${param}=${value}`
+  }
+
+  return query
+}
+
+const lastFm = async (method, params) => {
+  const _params = paramsToString(params)
+  let query = `?method=${method}&api_key=${config.LASTFM_API_KEY}&format=json`
+
+  const { data } = await axios
+    .get(config.LASTFM_API_URL + query + _params)
+
+  const methodName = method
+    .replace('get', '')
+    .split('.')
+    .pop()
+    .toLowerCase()
+
+  return data[methodName] || data
+}
+
+module.exports = { lastFm }
