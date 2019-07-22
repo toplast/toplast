25eec38a2cf4f27b51d09ba627254b5034bc5316
Henrique de Castilhos <henriquedecastilhos@gmail.com>
2019-07-22 00:22:04 -0300

    :construction: Creating footer chart

---
+++ src/components/chart/Footer.vue
@@ -0,0 +1,33 @@
+<template>
+  <VCard flat tile color="#fafafa" height="150">
+    <VLayout px-2 align-center fill-height>
+      <VFlex xs6 px-2 text-xs-center :style="`color: ${textColor};`">
+        <Item :data="data.left" :textColor="textColor" />
+      </VFlex>
+      <VFlex xs6 px-2 text-xs-center :style="`color: ${textColor};`">
+        <Item :data="data.right" :textColor="textColor" />
+      </VFlex>
+    </VLayout>
+  </VCard>
+</template>
+
+<script>
+import Item from '@/components/chart/footer/Item.vue'
+import bestContrast from 'get-best-contrast-color'
+
+export default {
+  name: 'ChartFooter',
+  props: {
+    data: { type: Object, default: () => {} },
+    colors: { type: Array, default: () => [] }
+  },
+  components: { Item },
+  computed: {
+    textColor: function () {
+      if (this.colors.length === 0)
+        return '#000'
+      return bestContrast('#f4f4f4', this.colors)
+    }
+  }
+}
+</script>
---
+++ src/components/chart/footer/Item.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-list-tile>
+    <v-list-tile-avatar size="75" color="grey darken-3">
+      <v-img
+        class="elevation-6"
+        src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
+      ></v-img>
+    </v-list-tile-avatar>
+
+    <v-list-tile-content class="ml-3">
+      <v-list-tile-title>Evan You</v-list-tile-title>
+      <v-list-tile-subtitle>Evan You</v-list-tile-subtitle>
+    </v-list-tile-content>
+  </v-list-tile>
+</template>
+
+<script>
+export default {
+  name: 'ChartFooterItem',
+  props: {
+    data: { type: Object, default: () => {} },
+    textColor: { type: String, default: '#000' }
+  }
+}
+</script>
