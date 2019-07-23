07dc7c517780e218d6c2912e313975bb0631e0dd
Henrique de Castilhos <henrique.castilhos@softexpert.com>
2019-07-23 14:31:34 -0300

    :wrench: Added max-len rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
-    'import/prefer-default-export': 'off'
+    'import/prefer-default-export': 'off',
+    'max-len': ['error', { 'code': 100 }]
   },
 };
