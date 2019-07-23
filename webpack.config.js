01a70e4e937b41ca41271286f71a78b4f4f89651
Henrique de Castilhos <henrique.castilhos@softexpert.com>
2019-07-23 14:50:27 -0300

    :wrench: Webpack external axios and spotify

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const webpack = require('webpack');
 const slsw = require('serverless-webpack');
 
-const { isLocal } = slsw.lib.webpack;
+// const { isLocal } = slsw.lib.webpack;
 
 module.exports = {
   target: 'node',
-  externals: ['aws-sdk'],
-  mode: isLocal ? 'development' : 'production',
+  externals: ['aws-sdk', 'axios', 'spotify-web-api-node'],
+  // mode: isLocal ? 'development' : 'production',
+  mode: 'production',
   entry: {
     ...slsw.lib.entries,
   },
