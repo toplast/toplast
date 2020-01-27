7e47d32c81fa1fba76604f2409bf8165e2ff7551
Henrique de Castilhos <henriquedecastilhos@gmail.com>
2020-01-27 12:07:32 -0300

    chore: removing old toplast

--- server/lib/config.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require('dotenv');
-
-const parsed = dotenv.config().parsed || {};
-
-module.exports = {
-  CLIENT_URL: parsed.CLIENT_URL || process.env.CLIENT_URL,
-  LAST_FM_API_URL: parsed.LAST_FM_API_URL || process.env.LAST_FM_API_URL,
-  LAST_FM_API_KEY: parsed.LAST_FM_API_KEY || process.env.LAST_FM_API_KEY
-};
