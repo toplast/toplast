2d0d1f1e5b49d06d404bf4165e5040962d82a80e
Henrique de Castilhos <henriquedecastilhos@gmail.com>
2020-03-02 16:17:01 -0300

    feature(server): adding get albums tests

---
+++ server/test/fixtures/topAlbums.ts
@@ -0,0 +1,50 @@
+export default {
+  topalbums: {
+    album: [
+      {
+        artist: {
+          url: "https://www.last.fm/music/Rodrigo+Zin",
+          name: "Rodrigo Zin",
+          mbid: "",
+        },
+        "@attr": {
+          rank: "4",
+        },
+        image: [
+          {
+            size: "small",
+            "#text":
+              "https://lastfm.freetls.fastly.net/i/u/34s/e91498885e168065470dda7e05dacd11.jpg",
+          },
+          {
+            size: "medium",
+            "#text":
+              "https://lastfm.freetls.fastly.net/i/u/64s/e91498885e168065470dda7e05dacd11.jpg",
+          },
+          {
+            size: "large",
+            "#text":
+              "https://lastfm.freetls.fastly.net/i/u/174s/e91498885e168065470dda7e05dacd11.jpg",
+          },
+          {
+            size: "extralarge",
+            "#text":
+              "https://lastfm.freetls.fastly.net/i/u/300x300/e91498885e168065470dda7e05dacd11.jpg",
+          },
+        ],
+        playcount: "675",
+        url:
+          "https://www.last.fm/music/Rodrigo+Zin/Fazendo+Grana+Pro+Meu+Filme",
+        name: "Fazendo Grana Pro Meu Filme",
+        mbid: "",
+      },
+    ],
+    "@attr": {
+      page: "1",
+      total: "4493",
+      user: "castilh0s",
+      perPage: "1",
+      totalPages: "4493",
+    },
+  },
+};
--- server/test/functions/getAlbums/index.test.ts
+++ server/test/functions/getAlbums/index.test.ts
@@ -1,40 +1,80 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable max-lines-per-function */
 /* eslint-disable @typescript-eslint/ban-ts-ignore */
+import LastFm from "@toplast/lastfm";
 import { handler } from "../../../functions/getAlbums";
+import topAlbums from "../../fixtures/topAlbums";
+
+jest.mock("@toplast/lastfm");
 
 describe("Get albums test", () => {
+  beforeAll(() => {
+    // @ts-ignore
+    LastFm.mockImplementation(() => {
+      return {
+        user: {
+          getTopAlbums: (): any => topAlbums,
+        },
+      };
+    });
+  });
+
+  beforeEach(() => {
+    // @ts-ignore
+    LastFm.mockClear();
+  });
+
   it("Should return error if user parameter is blank", async () => {
     // @ts-ignore
-    const albums = await handler({
+    const response = await handler({
       queryStringParameters: { user: "", limit: "5", period: "7day" },
     });
 
-    expect(albums).toEqual({
-      statusCode: 400,
-      body: JSON.stringify({ message: "Invalid parameters." }),
+    expect(response.statusCode).toEqual(400);
+    expect(JSON.parse(response.body)).toEqual({
+      message: "Invalid parameters.",
     });
   });
 
   it("Should return error if limit parameter is blank", async () => {
     // @ts-ignore
-    const albums = await handler({
+    const response = await handler({
       queryStringParameters: { user: "castilh0s", limit: "", period: "7day" },
     });
 
-    expect(albums).toEqual({
-      statusCode: 400,
-      body: JSON.stringify({ message: "Invalid parameters." }),
+    expect(response.statusCode).toEqual(400);
+    expect(JSON.parse(response.body)).toEqual({
+      message: "Invalid parameters.",
     });
   });
 
   it("Should return error if period parameter is blank", async () => {
     // @ts-ignore
-    const albums = await handler({
+    const response = await handler({
       queryStringParameters: { user: "castilh0s", limit: "5", period: "" },
     });
 
-    expect(albums).toEqual({
-      statusCode: 400,
-      body: JSON.stringify({ message: "Invalid parameters." }),
+    expect(response.statusCode).toEqual(400);
+    expect(JSON.parse(response.body)).toEqual({
+      message: "Invalid parameters.",
     });
+  });
+
+  it("Should return albums with status code 200", async () => {
+    // @ts-ignore
+    const response = await handler({
+      queryStringParameters: { user: "castilh0s", limit: "1", period: "7day" },
+    });
+
+    expect(response.statusCode).toEqual(200);
+    expect(JSON.parse(response.body)).toEqual([
+      {
+        artist: "Rodrigo Zin",
+        image:
+          "https://lastfm.freetls.fastly.net/i/u/300x300/e91498885e168065470dda7e05dacd11.jpg",
+        name: "Fazendo Grana Pro Meu Filme",
+        playcount: "675",
+      },
+    ]);
   });
 });
