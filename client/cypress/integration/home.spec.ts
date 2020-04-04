4d2694e4dfa029285877381024aaada3017b93bb
Henrique de Castilhos <henriquedecastilhos@gmail.com>
2020-04-03 23:42:19 -0300

    feat(tests): adds tests to header and footer

--- client/cypress/integration/home.spec.ts
+++ client/cypress/integration/home.spec.ts
@@ -1,9 +1,23 @@
 describe("Home page", () => {
-  it("Should shows title and description", () => {
+  beforeEach(() => {
     cy.visit("/");
 
     cy.url().should("equal", "http://localhost:3000/");
+  });
+
+  it("Should shows title and description", () => {
     cy.get("h1").should("have.text", "TopLast");
     cy.get("h2").should("have.text", "A Last.fm chart generator");
+  });
+
+  it("Should have be a header", () => {
+    cy.get("header").should("have.text", "TopLast");
+  });
+
+  it("Should have be a footer", () => {
+    cy.get("footer").should(
+      "have.text",
+      "Made with ❤️ by Castilhos and Munhoz",
+    );
   });
 });
