c3bdec8f03f4a3e5534e1cdd5bbb8959f2e8b6c0
Henrique de Castilhos <henriquedecastilhos@gmail.com>
2020-04-03 23:50:46 -0300

    refactor: uses header and footer in main

--- client/src/components/Main.tsx
+++ client/src/components/Main.tsx
@@ -1,19 +1,20 @@
-import { AppBar, Grid, Toolbar, Typography } from "@material-ui/core";
+import { Footer } from "./Footer/Footer.component";
+import { Grid } from "@material-ui/core";
+import { Header } from "./Header/Header.component";
 import React from "react";
 
 export const Main = ({ children }: { children: JSX.Element }): JSX.Element => {
   return (
     <React.Fragment>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6">TopLast</Typography>
-        </Toolbar>
-      </AppBar>
+      <Header />
+
       <Grid container>
         <Grid item xs={12}>
           {children}
         </Grid>
       </Grid>
+
+      <Footer />
     </React.Fragment>
   );
 };
