5fa3fb00315ebd68baec143d2655226f7980eda7
Henrique de Castilhos <henrique@castilhos.dev>
2021-01-06 18:29:07 -0300

    feat: add infographic component

---
+++ src/components/Infographic/Infographic.styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.main`
+  width: 750px;
+  height: 750px;
+`
---
+++ src/components/Infographic/index.ts
@@ -0,0 +1,8 @@
+import Body from './Body'
+import Footer from './Footer'
+import Header from './Header'
+import { Infographic } from './Infographic.component'
+
+export { Body, Footer, Header }
+export * from './Infographic.services'
+export default Infographic
